3b01a205138ccd81f4039041b24c4a2cd8cb8aa9
Mariano Mendez <marianomendez@fi.uba.ar>
2022-08-28 15:59:23 -0300

    Initial Commit

---
+++ Lox.java
@@ -0,0 +1,13 @@
+public class Lox {
+    public static void main(String[] args) throws Exception {
+        if (args.length>1){
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length==1){
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+}
