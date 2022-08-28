a57a019fb30ea87ea1757fe2eb2e125d031a05fe
Mariano Mendez <marianomendez@fi.uba.ar>
2022-08-28 17:09:38 -0300

    Scanner.java: scannToken() added               advance() addToken added               error handler added

--- core/Scanner.java
+++ core/Scanner.java
@@ -34,6 +34,40 @@ public class Scanner {
         return tokens;
     }       
 
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            //lexemas de un solo caracter
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+    
+            // manejo de errores
+            default:
+            Lox.error(line, "Unexpected character.");
+            break;
+        }
+    }
+
+    private char advance() {            // nuevo char!
+        return source.charAt(current++);
+    }
+        
+    private void addToken(TokenType type) { // agrega un token
+        addToken(type, null);
+    }
+    
+    private void addToken(TokenType type, Object literal) { // agrega un token con texto
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
 
 
     private boolean isAtEnd(){          // termino?
