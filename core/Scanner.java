633397d8dd41a029b999a7615cd329c2658f181e
Mariano Mendez <marianomendez@fi.uba.ar>
2022-08-28 16:59:46 -0300

    Scanner.java: added core function scanTokens()               current, lines and start added

--- core/Scanner.java
+++ core/Scanner.java
@@ -12,10 +12,32 @@ import static core.TokenType.*;
 public class Scanner {
     private final String source;                                    // stream de caracteres
     private final List<Token> tokens= new ArrayList<>();            // stream de tokens
+    private int start=0;        // inicio de la linea
+    private int current=0;      // posicion caracter actual
+    private int line=1;         // linea escaneada
 
 
     Scanner(String source) {
         this.source = source;
+    }
+
+    // El corazón del lexer
+    // escanea caracter a caracter 
+    
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+        // el inicio del proximo lexema .
+        start = current;
+        scanToken();
+        }
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }       
+
+
+
+    private boolean isAtEnd(){          // termino?
+        return current >=source.length();
     }
 
 
