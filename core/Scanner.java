f6b38478330735582a833292d04b5e9b70b8f3bf
Mariano Mendez <marianomendez@fi.uba.ar>
2022-08-28 19:16:22 -0300

    Scanner.java: reserved words , identifiers

--- core/Scanner.java
+++ core/Scanner.java
@@ -16,6 +16,27 @@ public class Scanner {
     private int current=0;      // posicion caracter actual
     private int line=1;         // linea escaneada
 
+    private static final Map<String, TokenType> keywords;
+
+    static {
+    keywords = new HashMap<>();
+    keywords.put("and",AND);
+    keywords.put("class",CLASS);
+    keywords.put("else",ELSE);
+    keywords.put("false",FALSE);
+    keywords.put("for",FOR);
+    keywords.put("fun",FUN);
+    keywords.put("if",IF);
+    keywords.put("nil",NIL);
+    keywords.put("or",OR);
+    keywords.put("print",PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super",SUPER);
+    keywords.put("this",THIS);
+    keywords.put("true",TRUE);
+    keywords.put("var",VAR);
+    keywords.put("while",WHILE);
+    }
 
     Scanner(String source) {
         this.source = source;
@@ -92,12 +113,23 @@ public class Scanner {
             default:
                 if (isDigit(c)) {
                     number();
+                }else if (isAlpha(c)) {
+                    identifier(); 
                 } else {
                     Lox.error(line, "Unexpected character.");
                 }
                 break;
         }
     }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
+    }
+        
 
     private void number() {
         while (isDigit(peek())) advance();
@@ -177,6 +209,15 @@ public class Scanner {
     // que un caracter sea un dígito
     private boolean isDigit(char c) {
         return c >= '0' && c <= '9';
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+        (c >= 'A' && c <= 'Z') ||
+        c == '_';
+    }
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);	
     }
 
 }
