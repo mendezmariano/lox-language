173e62bb367941297b5003ac822a6283cef23079
Mariano Mendez <marianomendez@fi.uba.ar>
2022-10-17 13:26:21 -0300

    Interpreter.java:Eval Parentesis

--- core/Interpreter.java
+++ core/Interpreter.java
@@ -6,9 +6,16 @@ public class Interpreter implements Visitor<Object> {
 
 
     @Override
-    public Object visitLiteralExpr(Expr.Literal expr) {
+    public Object visitLiteralExpr(Literal expr) {
         return expr.value;
     }
 
-    
+    @Override
+    public Object visitGroupingExpr(Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
 }
